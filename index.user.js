3c7b64b9db245c0bda1f975ffe072c3b307b2eec
iamfugui <61046396+iamfugui@users.noreply.github.com>
2024-07-28 14:32:50 +0800

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name         YouTube去广告 YouTube AD Blocker
+// @name         youtube-adb
 // @name:zh-CN   YouTube去广告
 // @name:zh-TW   YouTube去廣告
 // @name:zh-HK   YouTube去廣告
 // @name:zh-MO   YouTube去廣告
-// @namespace    https://github.com/iamfugui/YouTubeADB
-// @version      6.14
-// @description         这是一个去除YouTube广告的脚本，轻量且高效，它能丝滑的去除界面广告和视频广告，包括6s广告。This is a script that removes ads on YouTube, it's lightweight and efficient, capable of smoothly removing interface and video ads, including 6s ads.
-// @description:zh-CN   这是一个去除YouTube广告的脚本，轻量且高效，它能丝滑的去除界面广告和视频广告，包括6s广告。
-// @description:zh-TW   這是一個去除YouTube廣告的腳本，輕量且高效，它能絲滑地去除界面廣告和視頻廣告，包括6s廣告。
-// @description:zh-HK   這是一個去除YouTube廣告的腳本，輕量且高效，它能絲滑地去除界面廣告和視頻廣告，包括6s廣告。
-// @description:zh-MO   這是一個去除YouTube廣告的腳本，輕量且高效，它能絲滑地去除界面廣告和視頻廣告，包括6s廣告。
-// @author       iamfugui
+// @namespace    https://github.com/iamfugui/youtube-adb
+// @version      6.16
+// @description         A script to remove YouTube ads, including static ads and video ads, without interfering with the network and ensuring safety.
+// @description:zh-CN   脚本用于移除YouTube广告，包括静态广告和视频广告。不会干扰网络，安全。
+// @description:zh-TW   腳本用於移除 YouTube 廣告，包括靜態廣告和視頻廣告。不會干擾網路，安全。
+// @description:zh-HK   腳本用於移除 YouTube 廣告，包括靜態廣告和視頻廣告。不會干擾網路，安全。
+// @description:zh-MO   腳本用於移除 YouTube 廣告，包括靜態廣告和視頻廣告。不會干擾網路，安全。
 // @match        *://*.youtube.com/*
 // @exclude      *://accounts.youtube.com/*
 // @exclude      *://www.youtube.com/live_chat_replay*
